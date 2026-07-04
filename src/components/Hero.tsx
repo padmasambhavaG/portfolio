cfe401bb47343da55da00bdefd905dc550f537c0
padmasambhavaG <gopupadmasambhava@gmail.com>
2026-07-04 20:48:31 +0530

    chore: make bottom background gradient fade height responsive and lower to prevent excessive white bleed in light mode

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -132,7 +132,7 @@ export default function Hero() {
           className="absolute top-1/2 left-1/2 min-w-full min-h-full object-cover -translate-x-1/2 -translate-y-1/2 scale-110"
         />
         <div className="absolute inset-0 bg-black/20" />
-        <div className="absolute bottom-0 left-0 right-0 h-48 bg-gradient-to-t from-bg to-transparent" />
+        <div className="absolute bottom-0 left-0 right-0 h-20 md:h-32 bg-gradient-to-t from-bg to-transparent" />
       </motion.div>
 
       {/* Floating Navbar */}
